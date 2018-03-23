08bd8dbfc26e5ba8298c2235e6a99e63c7d356af
Mystery00 <mystery0dyl520@gmail.com>
2018-03-23 19:42:44 +0800

    add simple for crash handler

--- simple/src/main/java/vip/mystery0/manifestoftools/APP.java
+++ simple/src/main/java/vip/mystery0/manifestoftools/APP.java
@@ -1,11 +1,40 @@
 package vip.mystery0.manifestoftools;
 
 import android.app.Application;
+import android.os.Environment;
+
+import vip.mystery0.crashhandler.CatchException;
+import vip.mystery0.crashhandler.CrashHandler;
+import vip.mystery0.logs.Logs;
 
 public class APP extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-
+		CrashHandler.getInstance(this)
+				.setDir(Environment.getExternalStorageDirectory())
+				.setPrefix("crash")//设置日志文件名称头部
+				.setSuffix("txt")//设置日志文件扩展名
+				.autoClean(3)//设置自动清理频率
+				.doOnCatch(new CrashHandler.CatchExceptionListener() {
+					@Override
+					public void catchException(CatchException e) {
+						//捕获异常时调用
+					}
+				})
+//				.clean(new CrashHandler.AutoCleanListener() {
+//					@Override
+//					public void cleanDone() {
+//						//清理日志文件成功
+//					}
+//
+//					@Override
+//					public void cleanError(Exception e) {
+////					//清理日志文件失败
+//					}
+//				})
+				.init();
+		
+		Logs.setLevel(Logs.Level.DEBUG);//设置日志显示级别
 	}
 }
--- simple/src/main/java/vip/mystery0/manifestoftools/MainActivity.kt
+++ simple/src/main/java/vip/mystery0/manifestoftools/MainActivity.kt
@@ -2,13 +2,13 @@ package vip.mystery0.manifestoftools
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import vip.mystery0.tools.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 		setSupportActionBar(toolbar)
 
 		fab.setOnClickListener { view ->
+			toastMessage("这条消息来自BaseActivity#toastMessage")
 			Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 					.setAction("Action", null).show()
 		}
